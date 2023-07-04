5971ad1d0713de11a3c16cad6cf76e65fe286153
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-05 04:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 function decimalToBinary(decimal) {
-  if (decimal === 0) {
-    return '0';
-  }
-
-  let binary = '';
-
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
+  return decimal.toString(2);
 }
-
-const decimalNumber = 10;
-const binaryNumber = decimalToBinary(decimalNumber);
-
-console.log(binaryNumber);
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
